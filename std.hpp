eb7141f8b60d49390cd64c022e813f440c6135d3
Doxy <doxy@doxy.fyi>
2025-05-16 15:55:20 -0700

    Matrices
    Add support for (std::linalg powered) matrices.

--- std.hpp
+++ std.hpp
@@ -8,3 +8,34 @@
 		using namespace std::experimental;
 	}
 #endif
+
+#if __has_include(<mdspan>)
+	#include <mdspan>
+#else
+	#define MDSPAN_IMPL_STANDARD_NAMESPACE stdmath
+	#define MDSPAN_IMPL_PROPOSED_NAMESPACE experimental
+	#include "thirdparty/mdspan.hpp"
+
+	namespace stdmath {
+		using namespace stdmath::experimental;
+	}
+#endif
+
+#if __has_include(<linalg>)
+	#include <linalg>
+#else
+	#ifndef MDSPAN_IMPL_STANDARD_NAMESPACE
+	#define MDSPAN_IMPL_STANDARD_NAMESPACE stdmath
+	#define MDSPAN_IMPL_PROPOSED_NAMESPACE experimental
+	#endif
+	#include <__p1673_bits/linalg_config.h>
+	#include <experimental/linalg>
+
+	#ifndef MDSPAN_IMPL_STANDARD_NAMESPACE
+	namespace stdmath {
+		using namespace stdmath::experimental;
+	}
+	#endif
+#endif
+
+namespace stdmath { using namespace std; }
