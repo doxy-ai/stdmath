5baacbd81574ff1d251ee09d20e25327f29ff0b5
Doxy <doxy@doxy.fyi>
2025-11-02 18:34:03 -0800

    Matricies
    Add support for (std::linalg powered) matricies.

--- math/std.hpp
+++ math/std.hpp
@@ -16,8 +16,29 @@
 	#include <mdspan>
 #else
 	#define MDSPAN_IMPL_STANDARD_NAMESPACE stdmath
-	#define MDSPAN_IMPL_PROPOSED_NAMESPACE stl
+	#define MDSPAN_IMPL_PROPOSED_NAMESPACE experimental
 	#include "../thirdparty/mdspan.hpp"
+
+	namespace stdmath::stl {
+		using namespace stdmath::experimental;
+	}
+#endif
+
+#if __has_include(<linalg>)
+	#include <linalg>
+#else
+	#ifndef MDSPAN_IMPL_STANDARD_NAMESPACE
+	#define MDSPAN_IMPL_STANDARD_NAMESPACE stdmath
+	#define MDSPAN_IMPL_PROPOSED_NAMESPACE experimental
+	#endif
+	#include <__p1673_bits/linalg_config.h>
+	#include <experimental/linalg>
+
+	#ifndef MDSPAN_IMPL_STANDARD_NAMESPACE
+	namespace stdmath::stl {
+		using namespace stdmath::experimental;
+	}
+	#endif
 #endif
 
 #ifndef STD_MATH_DEFAULT_FLOAT_TYPE
