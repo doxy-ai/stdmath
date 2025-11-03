464254b0c43e78fbae833f8e13699f127f7db84f
Doxy <doxy@doxy.fyi>
2025-11-02 18:22:26 -0800

    Vectors
    Add support for (std::simd powered) vectors.
    
    Also sets up a basic unit testing framework

---
+++ math/vec.hpp
@@ -0,0 +1,275 @@
+#pragma once
+
+#include "std.hpp"
+#include <algorithm>
+#include <execution>
+#include <initializer_list>
+#include <ranges>
+#include <type_traits>
+
+namespace stdmath {
+	template<typename T, size_t N>
+	using simd_or_mask = std::conditional_t<std::is_same_v<T, bool>, stl::simd_mask<float, stl::simd_abi::fixed_size<N>>, stl::simd<T, stl::simd_abi::fixed_size<N>>>;
+
+	template<typename T, size_t N>
+	struct vec {
+		using self = vec;
+		using simd = simd_or_mask<T, N>;
+		using underlying_type = T;
+
+		alignas(stl::memory_alignment_v<simd>)
+		std::array<T, N> data;
+
+		constexpr vec(T broadcast) { data.fill(broadcast); }
+		constexpr vec(std::initializer_list<T> data) : data(data) {}
+		#include "../partials/vec1.constructors.partial.gen"
+
+		#include "../partials/vec.operators.partial"
+	};
+
+	template<typename T, size_t N>
+	struct swizzle {
+		using vec = stdmath::vec<T, N>;
+		using underlying_type = T;
+	};
+
+	template<typename T>
+	struct swizzle<T, 1> {
+		using underlying_type = T;
+		T* x;
+
+		#include "../partials/vec1.swizzles.partial.gen"
+
+		template<typename To>
+		using vec = stdmath::vec<To, 1>;
+		#include "../partials/swizzle.operators.partial"
+	};
+
+	template<typename T>
+	struct swizzle<T, 2> {
+		using underlying_type = T;
+		T *x, *y;
+
+		#include "../partials/vec2.swizzles.partial.gen"
+
+		template<typename To>
+		using vec = stdmath::vec<To, 2>;
+		#include "../partials/swizzle.operators.partial"
+	};
+
+	template<typename T>
+	struct swizzle<T, 3> {
+		using underlying_type = T;
+		T *x, *y, *z;
+
+		#include "../partials/vec3.swizzles.partial.gen"
+
+		template<typename To>
+		using vec = stdmath::vec<To, 3>;
+		#include "../partials/swizzle.operators.partial"
+	};
+
+	template<typename T>
+	struct swizzle<T, 4> {
+		using underlying_type = T;
+		T *x, *y, *z, *w;
+
+		#include "../partials/vec4.swizzles.partial.gen"
+
+		template<typename To>
+		using vec = stdmath::vec<To, 4>;
+		#include "../partials/swizzle.operators.partial"
+	};
+
+
+
+	template<typename T>
+	struct vec<T, 1> {
+		constexpr static size_t N = 1;
+		using self = vec;
+		using simd = simd_or_mask<T, N>;
+		using underlying_type = T;
+
+		union {
+			struct {
+				alignas(stl::memory_alignment_v<simd>)
+				T x;
+			};
+			alignas(stl::memory_alignment_v<simd>)
+			std::array<T, 1> data;
+		};
+
+		constexpr vec(T x) : x(x) {}
+		#include "../partials/vec1.constructors.partial.gen"
+
+		#include "../partials/vec.operators.partial"
+		#include "../partials/vec1.swizzles.address_of.partial.gen"
+	};
+
+	template<typename T>
+	template<typename To>
+	swizzle<T, 1>& swizzle<T, 1>::operator=(const vec<To>& vec) { *x = vec.x; return *this; }
+	template<typename T>
+	vec<T, 1> swizzle<T, 1>::to_vec() { return {*x}; }
+	template<typename T>
+	const vec<T, 1> swizzle<T, 1>::to_vec() const { return {*x}; }
+
+	template<typename T>
+	struct vec<T, 2> {
+		constexpr static size_t N = 2;
+		using self = vec;
+		using simd = simd_or_mask<T, N>;
+		using underlying_type = T;
+
+		union {
+			struct {
+				alignas(stl::memory_alignment_v<simd>)
+				T x;
+				T y;
+			};
+			alignas(stl::memory_alignment_v<simd>)
+			std::array<T, 2> data;
+		};
+
+		constexpr vec(T broadcast) : x(broadcast), y(broadcast) {}
+		constexpr vec(T x, T y) : x(x), y(y) {}
+		#include "../partials/vec2.constructors.partial.gen"
+
+		#include "../partials/vec.operators.partial"
+		#include "../partials/vec2.swizzles.address_of.partial.gen"
+	};
+
+	template<typename T>
+	template<typename To>
+	swizzle<T, 2>& swizzle<T, 2>::operator=(const vec<To>& vec) { *x = vec.x; *y = vec.y; return *this; }
+	template<typename T>
+	vec<T, 2> swizzle<T, 2>::to_vec() { return {*x, *y}; }
+	template<typename T>
+	const vec<T, 2> swizzle<T, 2>::to_vec() const { return {*x, *y}; }
+
+	template<typename T>
+	struct vec<T, 3> {
+		constexpr static size_t N = 3;
+		using self = vec;
+		using simd = simd_or_mask<T, N>;
+		using underlying_type = T;
+
+		union {
+			struct {
+				alignas(stl::memory_alignment_v<simd>)
+				T x;
+				T y;
+				T z;
+			};
+			struct {
+				alignas(stl::memory_alignment_v<simd>)
+				T r;
+				T g;
+				T b;
+			};
+			alignas(stl::memory_alignment_v<simd>)
+			std::array<T, 3> data;
+		};
+
+		constexpr vec(T broadcast) : x(broadcast), y(broadcast), z(broadcast) {}
+		constexpr vec(T x, T y, T z = {}) : x(x), y(y), z(z) {}
+		#include "../partials/vec3.constructors.partial.gen"
+
+		friend vec cross(const vec& a, const vec& b) {
+			return {
+				a.y * b.z - a.z * b.y,
+				a.z * b.x - a.x * b.z,
+				a.x * b.y - a.y * b.x
+			};
+		}
+
+		#include "../partials/vec.operators.partial"
+		#include "../partials/vec3.swizzles.address_of.partial.gen"
+	};
+
+	template<typename T>
+	template<typename To>
+	swizzle<T, 3>& swizzle<T, 3>::operator=(const vec<To>& vec) { *x = vec.x; *y = vec.y; *z = vec.z; return *this; }
+	template<typename T>
+	vec<T, 3> swizzle<T, 3>::to_vec() { return {*x, *y, *z}; }
+	template<typename T>
+	const vec<T, 3> swizzle<T, 3>::to_vec() const { return {*x, *y, *z}; }
+
+	template<typename T>
+	struct vec<T, 4> {
+		constexpr static size_t N = 4;
+		using self = vec;
+		using simd = simd_or_mask<T, N>;
+		using underlying_type = T;
+
+		union {
+			struct {
+				alignas(stl::memory_alignment_v<simd>)
+				T x;
+				T y;
+				T z;
+				T w;
+			};
+			struct {
+				alignas(stl::memory_alignment_v<simd>)
+				T r;
+				T g;
+				T b;
+				T a;
+			};
+			alignas(stl::memory_alignment_v<simd>)
+			std::array<T, 4> data;
+		};
+
+		constexpr vec(T broadcast) : x(broadcast), y(broadcast), z(broadcast), w(broadcast) {}
+		constexpr vec(T x, T y, T z = {}, T w = {}) : x(x), y(y), z(z), w(w) {}
+		#include "../partials/vec4.constructors.partial.gen"
+
+		#include "../partials/vec.operators.partial"
+		#include "../partials/vec4.swizzles.address_of.partial.gen"
+	};
+
+	template<typename T>
+	template<typename To>
+	swizzle<T, 4>& swizzle<T, 4>::operator=(const vec<To>& vec) { *x = vec.x; *y = vec.y; *z = vec.z; *w = vec.w; return *this; }
+	template<typename T>
+	vec<T, 4> swizzle<T, 4>::to_vec() { return {*x, *y, *z, *w}; }
+	template<typename T>
+	const vec<T, 4> swizzle<T, 4>::to_vec() const { return {*x, *y, *z, *w}; }
+
+	template<typename T, size_t N>
+	constexpr vec<T, N> normalize(const vec<T, N>& v) {
+		auto length = v.length();
+		return v / vec<T, N>{length};
+	}
+
+	template<typename T, size_t N>
+	constexpr vec<T, N> lerp(const vec<T, N>& a, const vec<T, N>& b, float t) {
+		return a * t + b * (1 - t);
+	}
+
+	template<size_t N>
+	inline bool all_of(const vec<bool, N>& v) {
+		return all_of(v.to_simd());
+	}
+	template<size_t N>
+	inline bool any_of(const vec<bool, N>& v) {
+		return any_of(v.to_simd());
+	}
+	template<size_t N>
+	inline bool none_of(const vec<bool, N>& v) {
+		return none_of(v.to_simd());
+	}
+	template<size_t N>
+	inline bool some_of(const vec<bool, N>& v) {
+		return some_of(v.to_simd());
+	}
+
+	template<typename T, size_t N>
+	constexpr bool approximately_equal(const vec<T, N>& a, const vec<T, N>& b, stl::float64_t epsilon = std::numeric_limits<stl::float32_t>::epsilon() * 100) {
+		for(size_t i = 0; i < N; ++i)
+			if(!approximately_equal(a.data[i], b.data[i], epsilon))
+				return false;
+		return true;
+	}
+}
---
+++ tests/vec.cpp
@@ -0,0 +1,237 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+#include <math/namespace_alias.hpp>
+
+
+TEST_CASE("vec<float, 1>") {
+	std::math::vec<float, 1> v1(1.0f);
+	CHECK(v1.x == 1.0f);
+
+	std::math::vec<float, 1> v2 = {2.0f};
+	CHECK(v2.x == 2.0f);
+
+	std::math::swizzle<float, 1> sw1 = v1._x();
+	CHECK(*sw1.x == 1.0f);
+	std::math::vec<float, 1> vec_from_swizzle = sw1.to_vec();
+	CHECK(vec_from_swizzle.x == 1.0f);
+}
+
+TEST_CASE("vec<float, 2>") {
+	std::math::vec<float, 2> v3(3.0f);
+	CHECK(v3.x == 3.0f);
+	CHECK(v3.y == 3.0f);
+
+	std::math::vec<float, 2> v4(4.0f, 5.0f);
+	CHECK(v4.x == 4.0f);
+	CHECK(v4.y == 5.0f);
+
+	std::math::swizzle<float, 2> sw2 = v4.xy();
+	CHECK(*sw2.x == 4.0f);
+	CHECK(*sw2.y == 5.0f);
+	std::math::vec<float, 2> vec_from_swizzle2 = sw2.to_vec();
+	CHECK(vec_from_swizzle2.x == 4.0f);
+	CHECK(vec_from_swizzle2.y == 5.0f);
+
+	CHECK(sw2.to_vec() == std::math::vec<float, 2>{4, 5});
+
+	std::math::vec<float, 2> normalized_vec_from_swizzle2 = normalize(sw2.to_vec());
+	CHECK(std::math::approximately_equal(normalized_vec_from_swizzle2.x, 0.624695));
+	CHECK(std::math::approximately_equal(normalized_vec_from_swizzle2.y, 0.780869));
+}
+
+TEST_CASE("vec<float, 3>") {
+	std::math::vec<float, 3> v5(6.0f);
+	CHECK(v5.x == 6.0f);
+	CHECK(v5.y == 6.0f);
+	CHECK(v5.z == 6.0f);
+
+	std::math::vec<float, 3> v6(7.0f, 8.0f);
+	CHECK(v6.x == 7.0f);
+	CHECK(v6.y == 8.0f);
+	CHECK(v6.z == 0);
+
+	std::math::vec<float, 3> v7(9.0f, 10.0f, 11.0f);
+	CHECK(v7.x == 9.0f);
+	CHECK(v7.y == 10.0f);
+	CHECK(v7.z == 11.0f);
+
+	std::math::vec<float, 3> cross_vec = cross(v6, v7);
+	CHECK(cross_vec.x == 88);
+	CHECK(cross_vec.y == -77);
+	CHECK(cross_vec.z == -2);
+
+	std::math::swizzle<float, 3> sw3 = v7.xyz();
+	CHECK(*sw3.x == 9.0f);
+	CHECK(*sw3.y == 10.0f);
+	CHECK(*sw3.z == 11.0f);
+
+	std::math::vec<float, 3> vec_from_swizzle3 = sw3.to_vec();
+	CHECK(vec_from_swizzle3.x == 9.0f);
+	CHECK(vec_from_swizzle3.y == 10.0f);
+	CHECK(vec_from_swizzle3.z == 11.0f);
+
+	CHECK(sw3.to_vec() == std::math::vec<float, 3>{9, 10, 11});
+
+	std::math::vec<float, 3> normalized_vec_from_swizzle3 = normalize(sw3.to_vec());
+	CHECK(std::math::approximately_equal(normalized_vec_from_swizzle3.x, 0.517892));
+	CHECK(std::math::approximately_equal(normalized_vec_from_swizzle3.y, 0.575435));
+	CHECK(std::math::approximately_equal(normalized_vec_from_swizzle3.z, 0.632979));
+}
+
+TEST_CASE("vec<float, 4>") {
+	std::math::vec<float, 4> v8(12.0f);
+	CHECK(v8.x == 12.0f);
+	CHECK(v8.y == 12.0f);
+	CHECK(v8.z == 12.0f);
+	CHECK(v8.w == 12.0f);
+
+	std::math::vec<float, 4> v9(13.0f, 14.0f);
+	CHECK(v9.x == 13.0f);
+	CHECK(v9.y == 14.0f);
+	CHECK(v9.z == 0);
+	CHECK(v9.w == 0);
+
+	std::math::vec<float, 4> v10(15.0f, 16.0f, 17.0f);
+	CHECK(v10.x == 15.0f);
+	CHECK(v10.y == 16.0f);
+	CHECK(v10.z == 17.0f);
+	CHECK(v10.w == 0);
+
+	std::math::vec<float, 4> v11(18.0f, 19.0f, 20.0f, 21.0f);
+	CHECK(v11.x == 18.0f);
+	CHECK(v11.y == 19.0f);
+	CHECK(v11.z == 20.0f);
+	CHECK(v11.w == 21.0f);
+
+	std::math::swizzle<float, 4> sw4 = v11.wxyz();
+	CHECK(*sw4.x == 21.0f);
+	CHECK(*sw4.y == 18.0f);
+	CHECK(*sw4.z == 19.0f);
+	CHECK(*sw4.w == 20.0f);
+
+	std::math::vec<float, 4> vec_from_swizzle4 = sw4.to_vec();
+	CHECK(vec_from_swizzle4.x == 21.0f);
+	CHECK(vec_from_swizzle4.y == 18.0f);
+	CHECK(vec_from_swizzle4.z == 19.0f);
+	CHECK(vec_from_swizzle4.w == 20.0f);
+
+	CHECK(sw4.to_vec() == std::math::vec<float, 4>{21.f, 18.f, 19.f, 20.f});
+
+	std::math::vec<float, 4> normalized_vec_from_swizzle4 = normalize(sw4.to_vec());
+	CHECK(std::math::approximately_equal(normalized_vec_from_swizzle4.x, 0.537579));
+	CHECK(stdmath::approximately_equal(normalized_vec_from_swizzle4.y, 0.460782));
+	CHECK(stdmath::approximately_equal(normalized_vec_from_swizzle4.z, 0.486381));
+	CHECK(stdmath::approximately_equal(normalized_vec_from_swizzle4.w, 0.51198));
+
+	auto v11x4 = v11 * 4;
+	CHECK(v11x4 == std::math::vec<float, 4>{18 * 4, 19 * 4, 20 * 4, 21 * 4});
+}
+
+TEST_CASE("vec<int, 1>") {
+	std::math::vec<int, 1> v1(1);
+	CHECK(v1.x == 1);
+
+	std::math::vec<int, 1> v2 = {2};
+	CHECK(v2.x == 2);
+
+	std::math::swizzle<int, 1> sw1 = v1._x();
+	CHECK(*sw1.x == 1);
+	std::math::vec<int, 1> vec_from_swizzle = sw1.to_vec();
+	CHECK(vec_from_swizzle.x == 1);
+}
+
+TEST_CASE("vec<int, 2>") {
+	std::math::vec<int, 2> v3(3);
+	CHECK(v3.x == 3);
+	CHECK(v3.y == 3);
+
+	std::math::vec<int, 2> v4(4, 5);
+	CHECK(v4.x == 4);
+	CHECK(v4.y == 5);
+
+	std::math::swizzle<int, 2> sw2 = v4.xy();
+	CHECK(*sw2.x == 4);
+	CHECK(*sw2.y == 5);
+	std::math::vec<int, 2> vec_from_swizzle2 = sw2.to_vec();
+	CHECK(vec_from_swizzle2.x == 4);
+	CHECK(vec_from_swizzle2.y == 5);
+}
+
+TEST_CASE("vec<int, 3>") {
+	std::math::vec<int, 3> v5(6);
+	CHECK(v5.x == 6);
+	CHECK(v5.y == 6);
+	CHECK(v5.z == 6);
+
+	std::math::vec<int, 3> v6(7, 8);
+	CHECK(v6.x == 7);
+	CHECK(v6.y == 8);
+	CHECK(v6.z == 0);
+
+	std::math::vec<int, 3> v7(9, 10, 11);
+	CHECK(v7.x == 9);
+	CHECK(v7.y == 10);
+	CHECK(v7.z == 11);
+
+	std::math::vec<int, 3> cross_vec = cross(v6, v7);
+	CHECK(cross_vec.x == 88);
+	CHECK(cross_vec.y == -77);
+	CHECK(cross_vec.z == -2);
+
+	std::math::swizzle<int, 3> sw3 = v7.xyz();
+	CHECK(*sw3.x == 9);
+	CHECK(*sw3.y == 10);
+	CHECK(*sw3.z == 11);
+
+	std::math::vec<int, 3> vec_from_swizzle3 = sw3.to_vec();
+	CHECK(vec_from_swizzle3.x == 9);
+	CHECK(vec_from_swizzle3.y == 10);
+	CHECK(vec_from_swizzle3.z == 11);
+}
+
+TEST_CASE("vec<int, 4>") {
+	std::math::vec<int, 4> v8(12);
+	CHECK(v8.x == 12);
+	CHECK(v8.y == 12);
+	CHECK(v8.z == 12);
+	CHECK(v8.w == 12);
+
+	std::math::vec<int, 4> v9(13, 14);
+	CHECK(v9.x == 13);
+	CHECK(v9.y == 14);
+	CHECK(v9.z == 0);
+	CHECK(v9.w == 0);
+
+	std::math::vec<int, 4> v10(15, 16, 17);
+	CHECK(v10.x == 15);
+	CHECK(v10.y == 16);
+	CHECK(v10.z == 17);
+	CHECK(v10.w == 0);
+
+	std::math::vec<int, 4> v11(18, 19, 20, 21);
+	CHECK(v11.x == 18);
+	CHECK(v11.y == 19);
+	CHECK(v11.z == 20);
+	CHECK(v11.w == 21);
+
+	std::math::swizzle<int, 4> sw4 = v11.wxyz();
+	CHECK(*sw4.x == 21);
+	CHECK(*sw4.y == 18);
+	CHECK(*sw4.z == 19);
+	CHECK(*sw4.w == 20);
+
+	std::math::vec<int, 4> vec_from_swizzle4 = sw4.to_vec();
+	CHECK(vec_from_swizzle4.x == 21);
+	CHECK(vec_from_swizzle4.y == 18);
+	CHECK(vec_from_swizzle4.z == 19);
+	CHECK(vec_from_swizzle4.w == 20);
+}
+
+TEST_CASE("vec<float, 4> -> vec<int, 4>") {
+	std::math::vec<float, 4> flt(18.1, 19.2, 20.3, 21.4);
+	std::math::vec<int, 4> integ = flt;
+	CHECK(integ.x == 18);
+	CHECK(integ.y == 19);
+	CHECK(integ.z == 20);
+	CHECK(integ.w == 21);
+}
