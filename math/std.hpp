e2b370d2405c60581db4ea3efe817db1ae4052c4
Doxy <doxy@doxy.fyi>
2025-11-02 18:22:26 -0800

    Vectors
    Add support for (optionally std::simd powered) vectors.
    
    Also sets up a basic unit testing framework.

---
+++ math/std.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <concepts>
+#if __has_include(<simd>)
+	#include <simd>
+#elif __has_include(<experimental/simd>)
+	#include <experimental/simd>
+	namespace stdmath::stl {
+		using namespace std::experimental;
+	}
+#else
+	#define STD_MATH_SIMD_UNAVAILABLE
+#endif
+
+#if __has_include(<mdspan>)
+	#include <mdspan>
+#else
+	#define MDSPAN_IMPL_STANDARD_NAMESPACE stdmath
+	#define MDSPAN_IMPL_PROPOSED_NAMESPACE stl
+	#include "../thirdparty/mdspan.hpp"
+#endif
+
+#ifndef STD_MATH_DEFAULT_FLOAT_TYPE
+#define STD_MATH_DEFAULT_FLOAT_TYPE stdmath::stl::float64_t
+#endif
+
+#include <stdfloat>
+namespace stdmath::stl { 
+	using namespace std;
+
+#ifndef __STDCPP_FLOAT32_T__
+	using float32_t = float;
+#endif
+#ifndef __STDCPP_FLOAT64_T__
+	using float64_t = double;
+#endif
+}
+
+namespace stdmath {
+	using float_t = STD_MATH_DEFAULT_FLOAT_TYPE;
+
+	inline static bool approximately_equal(stl::float64_t a, stl::float64_t b, stl::float64_t epsilon = std::numeric_limits<stl::float32_t>::epsilon() * 100) {
+		return std::abs<stl::float64_t>(a - b) < epsilon;
+	}
+}
