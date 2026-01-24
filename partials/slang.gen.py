4ceb0937fba4a1d7e6ef9491f04497287a286cb4
Doxy <doxy@doxy.fyi>
2026-01-23 23:58:20 -0800

    Slang

--- partials/slang.gen.py
+++ partials/slang.gen.py
@@ -23,14 +23,14 @@ def generate_hlsl_types():
 	for base in scalar_types:
 		for size in vector_sizes:
 			substitution = sub.get(base, base)
-			vector_types.append(f"using {base}{size} = vector<{substitution}, {size}>;")
+			vector_types.append(f"export using {base}{size} = vector<{substitution}, {size}>;")
 
 	# Generate matrix types (e.g. float4x4, double3x2)
 	for base in scalar_types:
 		for rows in matrix_sizes:
 			for cols in matrix_sizes:
 				substitution = sub.get(base, base)
-				matrix_types.append(f"using {base}{rows}x{cols} = matrix<{substitution}, {rows}, {cols}>;")
+				matrix_types.append(f"export using {base}{rows}x{cols} = matrix<{substitution}, {rows}, {cols}>;")
 
 	return vector_types + [''] + matrix_types
 
